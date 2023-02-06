0aecfb82be035bb05d25f00e7e2fb8e83cff3387
rexjoseph <josephrex96@gmail.com>
2023-02-06 09:32:13 -0500

    setup sale schema on backend

--- models/Product.js
+++ models/Product.js
@@ -28,6 +28,8 @@ const ProductSchema = new mongoose.Schema({
   color: { type: Array },
   price: { type: Number, required: true },
   inStock: { type: Boolean, default: true},
+  onSale: { type: Boolean, default: false },
+  fullPrice: { type: Number },
   rating: { type: Number, required: true, default: 0 },
   numReviews: { type: Number, required: true, default: 0 },
   reviews: [reviewSchema]
